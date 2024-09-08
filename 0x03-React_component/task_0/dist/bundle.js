a2a480965432e40776012fde0500767eebe456d6
afzidkarim <fzkarim@goodcomy.com>
2024-09-09 00:50:48 +0100

    done

---
+++ 0x03-React_component/task_0/dist/bundle.js
@@ -0,0 +1,2 @@
+##/*! For license information please see bundle.js.LICENSE.txt */
+(()=>{var e={963:(e,t,n)=>{"use strict";n.d(t,{Z:()=>i});var r=n(15),l=n.n(r),a=n(645),o=n.n(a)()(l());o.push([e.id,".course-list {\r\n  width: 95%;\r\n  margin: auto;
